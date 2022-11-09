b8b2c08f9f186c7d365fc6f0b1383117c7cf8714
ItsStillRyan <75793260+ItsStillRyan@users.noreply.github.com>
2022-11-09 21:17:59 +0800

    Removed old conflicting files

--- salary-management-backend/src/main/java/com/ryandevelopments/salarymanagement/util/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ryandevelopments.salarymanagement.util;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.ryandevelopments.salarymanagement.common.Employee;
-
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, String>{}
